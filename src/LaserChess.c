c9500c9530f5214da8e90d0577981af83facc592
Cyril Stoller <cyril.stoller@gmail.com>
2012-04-21 16:27:21 +0200

    decreased waiting time after easter egg 2 is finished

--- src/LaserChess.c
+++ src/LaserChess.c
@@ -764,7 +764,7 @@ void easter_egg1(void)
 			{
 				// Victorysound abspielen
 				play_sound(Victory);
-				WaitMs(6000);
+				WaitMs(4000);
 
 				// KeyPress Buffer löschen
 				while(IsKeyPressReady())
